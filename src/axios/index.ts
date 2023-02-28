9fb5be2e3b0e090a3cdbd3b58bc0f015f8dce592
annjolie <annjolie.lourido@gmail.com>
2023-02-28 21:37:24 +0100

    Adding connection to the podcast details

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,4 +1,5 @@
 import { URL } from '@/common/const/url';
+import { PodcastDetailType } from '@/common/types/podcastDetailsType';
 import { TopPodcastType } from '@/common/types/podcastType';
 import axios from 'axios';
 
@@ -17,10 +18,14 @@ const handleAxiosError = (error: Error): AxiosError => ({
   error,
 });
 
-export const getPodcasts = async (): Promise<
-  { data: TopPodcastType, error: null } | AxiosError
-> =>
+export const getPodcasts = async (): Promise<{ data: TopPodcastType; error: null } | AxiosError> =>
   instance
     .get('/us/rss/toppodcasts/limit=100/genre=1310/json')
+    .then(({ data }) => ({ data, error: null }))
+    .catch((error) => handleAxiosError(error));
+
+export const getPodcastDetail = async (podcastId: string): Promise<{ data: PodcastDetailType; error: null } | AxiosError> =>
+  instance
+    .get(`/lookup?id=${podcastId}&media=podcast&entity=podcastEpisode&limit=20`)
     .then(({ data }) => ({ data, error: null }))
     .catch((error) => handleAxiosError(error));
